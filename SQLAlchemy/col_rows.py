68b104c6b1c0e16ba44f2239968be4620f24af2b
Josh Lewis <jlewis200413@gmail.com>
2024-10-01 11:44:12 -0600

    Update and rename querying_data.py to col_rows.py

---
+++ SQLAlchemy/col_rows.py
@@ -0,0 +1,19 @@
+# Variable performing the SQL Query: SELECT username, comments FROM UserData
+spec_cols = session.query(UserData.username, UserData.comments).all()
+
+# Converting result from a list of dictionarys into a DataFrame
+df = pd.DataFrame(spec_cols)
+
+# Viewing result
+print(df)
+
+
+# Variable performing the SQL Query: SELECT * FROM UserData WHERE ID < 30. 
+# Effectively choosing the first 30 rows.
+spec_rows = session.query(UserData).filter(UserData.id <= 30).all()
+
+# Converting result from a list of dictionarys into a DataFrame
+df = pd.DataFrame(spec_rows)
+
+# Viewing result
+print(df)
