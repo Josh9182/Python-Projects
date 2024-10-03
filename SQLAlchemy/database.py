af9a9d131a8a084e074e5394680a00e30df0b10b
Josh Lewis <jlewis200413@gmail.com>
2024-10-03 11:01:09 -0600

    Update database.py

--- SQLAlchemy/database.py
+++ SQLAlchemy/database.py
@@ -1,6 +1,6 @@
-import pandas as pd 
-from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy import Column, Float, String, Integer, create_engine
+import pandas as pd
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+from sqlalchemy import create_engine, ForeignKey, Column, Float, String, Integer
 
 # CSV imports
 df = pd.read_csv("C:\\Users\\joshlewis\\Downloads\\ShinyApp\\user_data.csv")
@@ -8,11 +8,10 @@ odf = pd.read_csv("C:\\Users\\joshlewis\\Downloads\\ShinyApp\\occupation_data.cs
 
 # Accessing column names for both files imports
 columns = df.columns
-ocolumns = odf.columns
+Ocolumns = odf.columns
 
-# Accessing underlying data type for each column
 dtypes = df.dtypes
-odtypes = odf.dtypes
+Odtypes = odf.dtypes
 
 # Dictionary associating CSV dtype keys to SQLAlchemy dtype values  
 dtypes_dict = {
@@ -23,14 +22,18 @@ dtypes_dict = {
 # Foundation for class modeling
 Dbase = declarative_base()
 
-# "user_data" & "occupational_data" tables built upon the "Dbase", featuring row numbers
+# "user_data" & "occupational_data" tables built upon the "Dbase"
+# featuring dynamic row numbers and the ability to join via ForeignKey and relationship
 class UserData(Dbase):
     __tablename__ = "user_data"
     id = Column(Integer, primary_key= True, autoincrement= True)
+    occupations = relationship("OccData", back_populates= "users")
 
-class OccupationalData(Dbase):
-    __tablename__ = "occupational_data"
+class OccData(Dbase):
+    __tablename__ = "occ_data"
     id = Column(Integer, primary_key= True, autoincrement= True)
+    user_id = Column(Integer, ForeignKey('user_data.id'))
+    users = relationship("UserData", back_populates= "occupations")
 
 # Loop to convert CSV data types into ORM dtype and add into tables seperately
 for col in columns:
@@ -38,13 +41,14 @@ for col in columns:
     col_type = dtypes_dict.get(str_dtype, String)
     setattr(UserData, col, Column(col_type))
 
-for ocol in ocolumns:
-    str_dtype = str(odtypes[ocol])
+    
+for ocol in Ocolumns:
+    str_dtype = str(Odtypes[ocol])
     col_type = dtypes_dict.get(str_dtype, String)
-    setattr(OccupationalData, ocol, Column(col_type))
+    setattr(OccData, ocol, Column(col_type))
 
 # Initiate SQLite engine for database connection
-engine = create_engine("sqlite:///database.db")
+engine = create_engine("sqlite:///data_base.db")
 
 # Tables created, built off base and SQLite engine
 Dbase.metadata.create_all(engine)
@@ -55,12 +59,12 @@ smaker = sessionmaker(bind=engine)
 session = smaker()
 
 # Convert "df" & "odf" into list of dictionaries where each row is a dictionary. Key: Column, Value: Row Info
-ud_dict = df.to_dict(orient='records')
-od_dict = odf.to_dict(orient='records')
+ud_dict = df.to_dict(orient="records")
+od_dict = odf.to_dict(orient="records")
 
 # Insert dictionary list into "UserData" & "OccupationalData" table, filling in schema
 session.bulk_insert_mappings(UserData, ud_dict)
-session.bulk_insert_mappings(OccupationalData, od_dict)
+session.bulk_insert_mappings(OccData, od_dict)
 
 # Finalizing SQL transaction
 session.commit()
