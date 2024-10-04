17ba0d2feae7f9817c2650c87bcc79d43c963e47
Josh Lewis <jlewis200413@gmail.com>
2024-10-04 08:40:24 -0600

    Delete SQLAlchemy/spec_cols.py

--- SQLAlchemy/spec_cols.py
+++
@@ -1,8 +0,0 @@
-# Variable performing the SQL Query: SELECT username, comments FROM UserData
-spec_cols = session.query(UserData.username, UserData.comments).all()
-
-# Converting result from a list of dictionarys into a DataFrame
-df = pd.DataFrame(spec_cols)
-
-# Viewing result
-print(df)
